5fc7ae8f1309b3168d92c1bb27b31c94605b1a03
zdarov163 <75368249+zdarov163@users.noreply.github.com>
2021-01-11 00:06:45 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "student.cpp"
+
+
+
+int main()
+{
+    
+    Student obj1 = Student("0", "0", 0);
+    obj1.setStudent();
+    obj1.getStudent();
+    
+ 
+    return 0;
+}
+
---
+++ student.hpp
@@ -0,0 +1,19 @@
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
+
+#include <string>
+
+class Student
+{
+public:
+std::string nachname;
+std::string vorname;
+unsigned int matrikelnummer;
+
+Student(std::string nachname, std::string vorname, unsigned int matrikelnummer);
+
+void setStudent();
+void getStudent();
+};
+
+#endif
